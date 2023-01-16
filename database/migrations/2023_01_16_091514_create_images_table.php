4cca3818d4ce950eb3f182435a3b01a46bf9c686
MinhHieu20 <72988848+MinhHieu20@users.noreply.github.com>
2023-01-16 09:21:49 +0700

    feature21/Create migration images

---
+++ database/migrations/2023_01_16_091514_create_images_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('image')->nullable();
+            $table->text('title')->nullable();
+            $table->string('name')->nullable();
+            $table->string('link')->nullable();
+            $table->text('decs')->nullable();
+            $table->string('type')->nullable();
+            $table->integer('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+};
